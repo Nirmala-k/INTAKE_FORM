8d149cdb7eb462e805701abf5f962bc73f1bf9f8
Nirmala-k <nirmala@genepowerx.com>
2025-08-28 20:44:38 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -11,11 +11,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Serve frontend
-// app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(__dirname, "src")));
 
-// app.get(/.*/, (req, res) => {
-//   res.sendFile(path.join(__dirname, "../dist/index.html"));
-// });
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "src/index.html"));
+});
 
 dotenv.config();
 
@@ -223,6 +223,7 @@ app.get('/api/patients/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 

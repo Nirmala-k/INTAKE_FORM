90ebed9f91d325173b29334e74b3e405760f4a56
Nirmala-k <nirmala@genepowerx.com>
2025-09-01 10:07:18 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -209,3 +209,4 @@ app.get('/api/patients/:id', (req, res) => {
 });
 
 app.listen(5000, () => console.log('🚀 Server running on port 5000'));
+
